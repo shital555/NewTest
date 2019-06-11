9386a06793695fbc706b2f35c58ca298e14d28d5
shital555 <kashidshital555@gmail.com>
2019-06-11 16:21:18 +0530

    first time

---
+++ src/abc/cde/pqr/Abc.java
@@ -0,0 +1,16 @@
+package abc.cde.pqr;
+
+/**
+*				
+* shital
+*/
+
+public class Abc {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Inside abc");// TODO Auto-generated method stub
+
+	}
+
+}
